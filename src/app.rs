286f6e6a8f9ee3a01e1718c3b7c5634d76cb888c
Felix Salcher <dev.felix.salcher@gmail.com>
2024-11-23 09:50:39 +0100

    added simple structure

---
+++ src/app.rs
@@ -0,0 +1,60 @@
+use std::{
+    io::{self, Stdout},
+    time::Duration,
+};
+
+use ratatui::{
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    prelude::{Buffer as TBuffer, CrosstermBackend, Rect},
+    widgets::{Paragraph, Widget},
+    Frame, Terminal,
+};
+
+pub struct App {
+    exit: bool
+}
+
+impl App {
+    pub fn new() -> Self {
+        App {
+            exit: false
+        }
+    }
+
+    pub fn run(&mut self, terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
+        }
+
+        Ok(())
+    }
+
+    fn render_frame(&mut self, frame: &mut Frame) {
+        self.render(frame.area(), frame.buffer_mut());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        if event::poll(Duration::from_millis(10))? {
+            match event::read()? {
+                Event::Key(event) if event.kind == KeyEventKind::Press => self.handle_keys(event) ,
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_keys(&mut self, event: KeyEvent) {
+        match event.code {
+            KeyCode::Char('q') => self.exit = true,
+            _ => {}
+        }
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut TBuffer) {
+        Paragraph::new("hi there").render(area, buf);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+use std::io;
+
+use app::App;
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::new().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
