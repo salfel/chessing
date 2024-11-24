215e888cee6461228c163ab018472f69b51615da
Felix Salcher <dev.felix.salcher@gmail.com>
2024-11-24 01:04:15 +0100

    added board initialization

--- server/internal/server/board.go
+++ server/internal/server/board.go
@@ -1,7 +1,60 @@
 package server
 
-type Board struct{}
+import "fmt"
+
+type Field struct {
+	x int
+	y int
+}
+
+func (f *Field) String() string {
+	return fmt.Sprintf("%c%d", byte('A'+f.x), f.y)
+}
+
+type Board struct {
+	pieces map[Field]Piece
+}
 
 func newBoard() Board {
-	return Board{}
+	pieces := make(map[Field]Piece)
+
+	for i := range 8 {
+		field := Field{x: i, y: 2}
+		pieces[field] = WHITEPAWN
+
+		field.y = 7
+		pieces[field] = BLACKPAWN
+	}
+
+	pieces[Field{x: 0, y: 1}] = WHITEROOK
+	pieces[Field{x: 7, y: 1}] = WHITEROOK
+
+	pieces[Field{x: 0, y: 8}] = BLACKROOK
+	pieces[Field{x: 7, y: 8}] = BLACKROOK
+
+	pieces[Field{x: 1, y: 1}] = WHITEKNIGHT
+	pieces[Field{x: 6, y: 1}] = WHITEKNIGHT
+
+	pieces[Field{x: 1, y: 8}] = BLACKKNIGHT
+	pieces[Field{x: 6, y: 8}] = BLACKKNIGHT
+
+	pieces[Field{x: 2, y: 1}] = WHITEBISHOP
+	pieces[Field{x: 5, y: 1}] = WHITEBISHOP
+
+	pieces[Field{x: 2, y: 8}] = BLACKBISHOP
+	pieces[Field{x: 5, y: 8}] = BLACKBISHOP
+
+	pieces[Field{x: 3, y: 1}] = WHITEQUEEN
+	pieces[Field{x: 4, y: 1}] = WHITEKING
+
+	pieces[Field{x: 3, y: 8}] = BLACKQUEEN
+	pieces[Field{x: 4, y: 8}] = BLACKKING
+
+	for field, piece := range pieces {
+		fmt.Printf("%s = %c\n", field.String(), piece.variant)
+	}
+
+	return Board{
+		pieces: pieces,
+	}
 }
